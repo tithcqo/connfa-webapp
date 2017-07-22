6c697c40f3926a0b3ecfc7608ecaebb07ee51fad
tithcqo <tithcqo@yahoo.com>
2017-07-22 07:56:56 +0100

    Updated

--- src/app/config/config.example.ts
+++ src/app/config/config.example.ts
@@ -1,10 +1,10 @@
 export const CONFIG = {
     development: {
-        "apiUrl": "API Base URL",
-        "googleApiKey": "SET YOUR GOOGLE API KEY HERE",
+        "apiUrl": "http://ican.thenicetreat.com/api/v2/new-pierceberg-2017/",
+        "googleApiKey": "AIzaSyCr_jT0aS83X4IJ-mF8yF4cRG3Tq445XZY",
     },
     production: {
-        "apiUrl": "",
-        "googleApiKey": "",
+        "http://ican.thenicetreat.com/api/v2/new-pierceberg-2017/": "",
+        "googleApiKey": "AIzaSyCr_jT0aS83X4IJ-mF8yF4cRG3Tq445XZY",
     }
 };
